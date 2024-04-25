3a8d3b7521ec84d0300bb7cc41066b24b5f71648
mattmacf98 <macfarqu@amazon.com>
2024-04-25 16:50:12 -0400

    fix deploy script

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -2,5 +2,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 heroku container:login
-heroku container:push web --ap mattmacf98-distnode
-heroku container:release web --ap mattmacf98-distnode
+heroku container:push web --app mattmacf98-distnode
+heroku container:release web --app mattmacf98-distnode
